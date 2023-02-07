6a6ae9c220d35e84fda4417771b3bdb560472e19
antonio moschetti <antoniomoschetti@hotmail.it>
2023-02-07 19:18:20 +0000

    changed routes

--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_183733) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.bigint "donut_id", null: false
+    t.bigint "user_id", null: false
+    t.integer "quantity"
+    t.integer "total_cost"
+    t.string "status"
+    t.boolean "complete"
+    t.date "requested_date"
+    t.time "requested_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["donut_id"], name: "index_orders_on_donut_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
     t.string "comment"
@@ -80,4 +95,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_183733) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "orders", "donuts"
+  add_foreign_key "orders", "users"
 end
