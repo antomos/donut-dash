861200b16884d5cfd20c1e9b83e4c06e25af41b6
antonio moschetti <antoniomoschetti@hotmail.it>
2023-02-08 13:20:29 +0000

    Adds donut views

--- db/schema.rb
+++ db/schema.rb
@@ -47,10 +47,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_192232) do
     t.string "description"
     t.float "price"
     t.boolean "available"
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_donuts_on_user_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -97,7 +95,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_192232) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "donuts", "users"
   add_foreign_key "orders", "donuts"
   add_foreign_key "orders", "users"
 end
