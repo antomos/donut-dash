14eb126006a883c3efa80ee9261a7f7e426a23e9
antonio moschetti <antoniomoschetti@hotmail.it>
2023-02-10 13:46:56 +0000

    updated donut model and seed with rating

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,8 +35,10 @@ FILLING = [ "Chocolate", "Glazed", "Sugar", "Cinnamon", "Maple", "Blueberry", "S
     donut.user = user
     donut.save!
     3.times do
-      review= Review.create!(comment:Faker::Movie.quote, rating:rand(6) ,donut:donut)
+      review= Review.create!(comment:Faker::Movie.quote, rating:rand(1..5) ,donut:donut)
       puts "Review #{review.id} created for donut #{donut.id}"
+      donut.rating = Review.where(donut: donut).average(:rating).round(2)
+      donut.save!
     end
     puts "Donut #{donut.id} created"
   end
