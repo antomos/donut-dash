d153f9f0034b55dc2c97c1c6ca1164149c25813d
lewis schaefer <lewis.schaefer@hotmail.co.uk>
2023-02-13 16:00:44 +0000

    Addsincomping orders to bakery page with options to change status and set order complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :donuts, only: [:index, :show, :new, :create] do
-    resources :orders, only: [:new, :create]
+    resources :orders, only: [:new, :create, :new, :update]
     resources :reviews, only: [:create]
   end
   resources :orders, only: [:index, :show]
