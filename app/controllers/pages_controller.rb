64ff4de923507e7bbc76b80e121f00ea3bc3e69c
lewis schaefer <lewis.schaefer@hotmail.co.uk>
2023-02-16 12:36:23 +0000

    Brings order sort logic to pages bakery controller method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,5 +8,19 @@ class PagesController < ApplicationController
   end
 
   def bakery
+    @complete_orders = []
+    @pending_orders = []
+    @accepted_orders = []
+
+    current_user.donuts.each do |donut|
+      donut.orders.each do |order|
+        if order.complete
+          @complete_orders << order
+        else
+          @pending_orders << order if order.status == "pending"
+          @accepted_orders << order if order.status == "accepted"
+        end
+      end
+    end
   end
 end
