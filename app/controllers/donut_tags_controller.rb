6621b5968825acb7d902debb52e5d3dea66e4788
lewis schaefer <lewis.schaefer@hotmail.co.uk>
2023-02-17 11:54:07 +0000

    adds basic tag system - new tag link broken

---
+++ app/controllers/donut_tags_controller.rb
@@ -0,0 +1,25 @@
+class DonutTagsController < ApplicationController
+  before_action :set_donut, only: [:new, :create]
+  def new
+    @donut_tag = DonutTag.new()
+  end
+
+  def create
+    @donut_tag = DonutTag.new(donut_tag_params)
+    @donut_tag.donut = @donut
+
+    if @donut_tag.save
+      redirect_to bakery_path
+    end
+  end
+
+  private
+
+  def set_donut
+    @donut = Donut.find(params[:donut_id])
+  end
+
+  def donut_tag_params
+    params.require(:donut_tag).permit(:tag_id, :donut_id)
+  end
+end
